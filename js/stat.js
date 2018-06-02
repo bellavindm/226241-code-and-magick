daa562cd5d09e0eabc774ef5c5fd6dfcfc8b20f3
Dima Bellavin <izume@inbox.ru>
2018-06-02 19:35:29 +0300

    Канвас

---
+++ js/stat.js
@@ -0,0 +1,56 @@
+'use strict';
+
+(function() {
+
+window.renderStatistics = function (ctx, names, times) {
+  ctx.fillStyle = 'rgba(0, 0, 0, 0.7)';
+  ctx.fillRect(160, 20, 420, 270);
+
+  ctx.fillStyle = 'rgba(255, 255, 255, 1.0)';
+  ctx.fillRect(150, 10, 420, 270);
+
+  ctx.fillStyle = '#000';
+  ctx.font = '16px PT Mono';
+
+  ctx.fillText('Ура вы победили!\nСписок результатов:', 170, 40);
+  // ctx.fillText('Список результатов:', 170, 60);
+
+  var max = -1;
+  var maxIndex = -1;
+
+  for (var i = 0; i < times.length; i++) {
+    if (times[i] > max) {
+      max = times[i];
+      maxIndex = i;
+    }
+  }
+
+  var histogramHeight = 150;
+  var step = histogramHeight / (max - 0);
+
+  ctx.fillText('Худшее время: ' + Math.floor(max) + 'мс у игрока ' +
+    names[maxIndex], 170, 80);
+
+  var barHeight = 50;  //px Ширина одной полоски
+  var indent = 110;    //px Отступ между полосками
+  var initialX = 170;  //px Начальная координата X
+  var initialY = 100;  //px Начальная координата Y
+
+  for (var i = 0; i < times.length; i++) {
+    if (names[i] === 'Вы') {
+      ctx.fillStyle = 'rgba(255, 0, 0, 1)';
+    } else {
+      ctx.fillStyle = 'rgba(0, 0, 255, ' + Math.random() + ')';
+    }
+    ctx.fillRect(initialX + indent * i,                  //x
+      histogramHeight - times[i] * step + initialY,      //y
+        barHeight,                                       //width
+          times[i] * step);                              //height
+
+    ctx.fillStyle = 'Black';
+    ctx.fillText(names[i], initialX + indent * i + 5,
+      initialY + histogramHeight + 20);
+  }
+};
+
+})();
